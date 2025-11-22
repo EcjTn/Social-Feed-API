2afd060a31f747fa1cf1fd639b71091a76078681
EcjTn <ecjtaneo@gmail.com>
2025-11-22 16:02:06 +0800

    feat(follow): added getting current user's followers

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -5,23 +5,27 @@ import type { IJwtPayload } from 'src/common/interfaces/jwt-payload.interface';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { parseCursor } from 'src/utils/cursor-parser.utils';
 
-@Controller('/users/:username/')
+@Controller('/users')
 @UseGuards(JwtAuthGuard)
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
 
-  @Get('/followers')
-  public async getUsersFollowers(@Param('username')username: string, @Query('cursor')cursor?: string) {
-    const parsedCursor = parseCursor(cursor)
-    return await this.followService.getFollowers({username}, parsedCursor)
+  @Get('/me/followers')
+  public async getMyFollowers(@User()user: IJwtPayload, @Query('cursor')cursor?: string) {
+    return this.followService.getFollowers({userId: user.sub}, parseCursor(cursor))
   }
 
-  @Post('/follow')
+  @Get('/:username/followers')
+  public async getUsersFollowers(@Param('username')username: string, @Query('cursor')cursor?: string) {
+    return await this.followService.getFollowers({username}, parseCursor(cursor))
+  }
+
+  @Post('/:username/follow')
   public async followUser(@User()user: IJwtPayload, @Param('username')username: string) {
     return await this.followService.follow(user.sub, username)
   }
 
-  @Delete('/unfollow')
+  @Delete('/:username/unfollow')
   public async unfollowUser(@User()user: IJwtPayload, @Param('username')username: string) {
     return await this.followService.unfollow(user.sub, username)
   }
