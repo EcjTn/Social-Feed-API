c5c57dcf3ca3d796ab52d7c4ef84641369d6af5a
EcjTn <ecjtaneo@gmail.com>
2025-11-26 08:59:27 +0800

    enabled gzip back

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //app.use(compression())
+  app.use(compression())
   app.use(cors())
   app.use(cookieParser())
   app.use(helmet())
