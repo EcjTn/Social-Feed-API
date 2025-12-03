94a07c68325bd97ce68347b68d0720fbfaebf28c
EcjTn <ecjtaneo@gmail.com>
2025-12-03 17:16:12 +0800

    swagger dto fixed

--- src/staff/dtos/set-role.dto.ts
+++ src/staff/dtos/set-role.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum } from "class-validator";
 import { UserRole } from "src/common/enums/user-role.enum";
 
 export class SetRoleDto {
+    @ApiProperty({ enum: UserRole, description: 'The role to be assigned to the user.' })
     @IsEnum(UserRole, { message: 'role must be a valid role(admin, moderator, user).' })
     role: UserRole;
 }
