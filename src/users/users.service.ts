fe7f03c08969955804708f6d5aeee6602b782ea5
EcjTn <ecjtaneo@gmail.com>
2025-12-05 14:34:44 +0800

    fixed var name

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,7 +17,7 @@ export class UsersService {
         @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     ) { }
 
-    private readonly hisoryLimit = 5
+    private readonly historyLimit = 5
 
     public async search(username: string, cursor?: number) {
         const limit = 10
